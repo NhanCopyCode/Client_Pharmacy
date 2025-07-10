644b8335fc8b9bb9c151aef0d322f3408a977aaf
thanhnhanggg <lephuocthanhnhat0409@gmail.com>
2025-07-10 23:22:49 +0700

    feat(overlay client): fix display overlay clinet

--- src/components/Client/OverlayGray.jsx
+++ src/components/Client/OverlayGray.jsx
@@ -1,7 +1,7 @@
-function OverlayGray({ onClick = () => {}, zIndex = 50 }) {
+function OverlayGray({ onClick = () => {}, zIndex = 10 }) {
 	return (
 		<div
-			className={`fixed inset-0 bg-black !opacity-50 z-[${zIndex}]`}
+			className={`fixed inset-0 bg-black !opacity-50 z-${zIndex}`}
 			onClick={onClick}
 		/>
 	);
